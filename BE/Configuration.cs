c8de09337ae1af1f166877a786f629bc6091aaf3
איתן גולדשמידט <eitntt@gmail.com>
2018-12-10 21:31:24 +0200

    added Configuration (not final)

---
+++ BE/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BE
+{
+    public class Configuration
+    {
+        public const int MIN_LESSONS = 27;
+        public const int MAX_TESTER_AGE = 80;
+        public const int MIN_STUDENT_AGE = 17;
+        public const int MIN_DAYS_BETWEEN_TESTS = 7;
+    }
+}
