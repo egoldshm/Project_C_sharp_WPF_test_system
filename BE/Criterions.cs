2df05938114a2a8110e13616c5815850943dd06f
איתן גולדשמידט <eitntt@gmail.com>
2018-12-10 22:32:13 +0200

    added Criterions Test and Trainee Classes. only the basic... need more addtions! Darshan, I need your help. about my english and in general...

---
+++ BE/Criterions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BE
+{
+    enum Criterion
+    {
+        passed, Fails
+    }
+    public class CriterionsOfTest
+    {
+        private Criterion keepingDistants;
+        private Criterion reverseParking;
+        private Criterion lookingAtTheMirror;
+        private Criterion semaphors;
+
+
+
+    }
+}
