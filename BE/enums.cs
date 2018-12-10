bf984e8d15e95d4c47f94b1a9d48a759ab9f7e00
איתן גולדשמידט <eitntt@gmail.com>
2018-12-10 21:18:41 +0200

    add enums

---
+++ BE/enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BE
+{
+    enum CarType
+    {
+        Private_Car, Two_wheeled_vehicles, Medium_truck, Heavy_truck
+    }
+    enum giltBoxType
+    {
+        Manual, Automatic
+    }
+    enum Gender
+    {
+        Male, Female
+    }
+
+}
