3a0eddc9ed43c6e129a6accd71f2727f8bad3474
ardarsha <fortunnlebear@gmail.com>
2018-12-10 21:30:35 +0200

    Added the Tester class, with some of the variables needed.

---
+++ BE/Tester.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Tester
+{
+    private int id;//has to appear
+    private string lastName;
+    private string firstName;
+    private DateTime dateOfBirth;
+    private Gender gender;
+    private long phoneNumber;
+    private string address;
+    private int yearsOfExperience;
+    private int maxWeeklyTests;
+    private CarType carType;
+    public Tester()
+    {
+    }
+}
