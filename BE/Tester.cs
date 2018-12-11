6e1b8a477c5acba7a1b84f20913ddeb9a73bdcbe
ardarsha <fortunnlebear@gmail.com>
2018-12-11 13:00:11 +0200

    Done with Tester, according to the pdf

--- BE/Tester.cs
+++ BE/Tester.cs
@@ -1,27 +1,28 @@
 ﻿using System;
-
-public class Tester
-{
-    private int id;//has to appear
-    private string? lastName;
-    private string? firstName;
-    private DateTime dateOfBirth;
-    private Gender gender;
-    private long phoneNumber;
-    private string address;
-    private int yearsOfExperience;
-    private int maxWeeklyTests;
-    private CarType carType;
-    private bool [,] workDays = new bool [5, 6];
-    private float maxDistance;
-
-    public Tester()
+namespace BE{
+    public class Tester
     {
-
+        private int id;//has to appear
+        private string lastName;
+        private string firstName;
+        private DateTime dateOfBirth;
+        private Gender gender;
+        private long phoneNumber;
+        private string address;
+        private int yearsOfExperience;
+        private int maxWeeklyTests;
+        private CarType carType;
+        private bool [,] workDays = new bool [5, 6];
+        private float maxDistance;
+    
+        public Tester()
+        {
+            
+        }
+    
+        public override string ToString()
+        {
+                return firstName + " " + familyName;
+        }
     }
-
-    public override string ToString()
-    {
-            return firstName + " " + familyName;
-     }
 }
