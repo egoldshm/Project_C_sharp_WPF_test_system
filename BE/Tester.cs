e19d14f841848c6d2f50e1f1a666deb70a188a6a
ardarsha <fortunnlebear@gmail.com>
2018-12-12 00:59:36 +0200

    added properties to Tester class

--- BE/Tester.cs
+++ BE/Tester.cs
@@ -20,18 +20,25 @@ namespace BE{
         {
             
         }
-    
+
+        
+        public int Id { get => id; set => id = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public DateTime DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+        public Gender Gender { get => gender; set => gender = value; }
+        public long PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Address { get => address; set => address = value; }
+        public int YearsOfExperience { get => yearsOfExperience; set => yearsOfExperience = value; }
+        public int MaxWeeklyTests { get => maxWeeklyTests; set => maxWeeklyTests = value; }
+        public CarType CarType { get => carType; set => carType = value; }
+        public bool[,] WorkDays { get => workDays; set => workDays = value; }
+        public float MaxDistance { get => maxDistance; set => maxDistance = value; }
+        
         public override string ToString()
         {
                 return firstName + " " + familyName;
         }
     }
-<<<<<<< HEAD
-=======
 
-    public override string ToString()
-    {
-            return firstName + " " + familyName;
-     }
->>>>>>> 9ea54a4ebfaf888ec80f31df4ec97a76beee124d
 }
