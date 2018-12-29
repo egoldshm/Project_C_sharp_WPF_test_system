8f132f8dfdc2ea3ab658a3a7cafd5728daed27b9
ardarsha <fortunnlebear@gmail.com>
2018-12-30 01:50:05 +0200

    added criteria check in bl

--- BE/Configuration.cs
+++ BE/Configuration.cs
@@ -10,6 +10,7 @@ namespace BE
         public const int MIN_LESSON_BEFORE_TEST = 27;
         public const int MIN_TESTER_AGE = 30;
         public const int DURATION_OF_TEST = 30;
+        public const float PERCETAGE_REQUIRED_FOR_PASSING = 80;
 
         public static readonly string[] TYPE_OF_CRITERIONS = new string[] { "keeping distance"
             ,"reverse parking","looking at The mirror"
