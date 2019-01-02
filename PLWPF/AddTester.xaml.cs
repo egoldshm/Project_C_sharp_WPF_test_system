71a930f83290bfc9f2f19dc97744599ad9b9ef1b
ardarsha <fortunnlebear@gmail.com>
2019-01-02 22:19:08 +0200

    Added admin and add tester windows

---
+++ PLWPF/AddTester.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for AddTester.xaml
+    /// </summary>
+    public partial class AddTester : UserControl
+    {
+        
+        public AddTester()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    CheckBox checkBox = new CheckBox();
+                    WorkHours.Children.Add(checkBox);
+                    Grid.SetRow(checkBox, i);
+                    Grid.SetColumn(checkBox, j);
+                    checkBox.Margin = new Thickness(3);
+                    checkBox.Name = "checkBox" + i + "_" + j;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ PLWPF/AdminMainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Ibl;
+using BE;
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for TraineeMainWindow.xaml
+    /// </summary>
+    public partial class AdminMainWindow : Window
+    {
+        IBL bl = factoryBL.FactoryBL.GetBL();
+        public AdminMainWindow(User user)
+        {
+            if (user.role != User.RoleTypes.Admin)
+            {
+                throw new Exception("user isn't admin");
+            }
+            InitializeComponent();
+            welcomeMessage.Content = "Welcome " + user.Username;
+        }
+
+        private void viewTest(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void viewTestYouPassed(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void addTestToTrainee(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
