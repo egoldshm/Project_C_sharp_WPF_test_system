334303a4e2c71a513dc6f65df6c65e74c7ed2573
ardarsha <fortunnlebear@gmail.com>
2019-01-03 05:00:58 +0200

    added a search functionality in trainee window, made the checkboxes be filters, and not the determening factor for the result pool

--- PLWPF/ViewTrainees.xaml.cs
+++ PLWPF/ViewTrainees.xaml.cs
@@ -23,11 +23,13 @@ namespace PLWPF
     {
         IBL bl = factoryBL.FactoryBL.GetBL();
         public List<Trainee> trainees;
+        public List<Trainee> ToDisplay;
         public ViewTrainees()
         {
             InitializeComponent();
             trainees = new List<Trainee>(bl.GetAllTrainees());
-            list.DataContext = trainees;
+            ToDisplay = trainees;
+            list.DataContext = ToDisplay;
 
         }
 
@@ -35,21 +37,72 @@ namespace PLWPF
         {
             if(ShowNoLicense.IsChecked == true && ViewLicenseOwners.IsChecked == true)
             {
-                trainees = bl.GetAllTrainees();
+                ToDisplay = trainees;
             }
             else if (ShowNoLicense.IsChecked == false && ViewLicenseOwners.IsChecked == true)
             {
-                trainees = bl.GetAllTraineesByLicense(true);
+                ToDisplay = new List<Trainee>(trainees.Where(tr => bl.GetAllTraineesByLicense(true).Any(tr2 => tr.Id == tr2.Id)));
             }
             else if(ShowNoLicense.IsChecked == true && ViewLicenseOwners.IsChecked == false)
             {
-                trainees = bl.GetAllTraineesByLicense(false);
+                ToDisplay = new List<Trainee>(trainees.Where(tr => bl.GetAllTraineesByLicense(false).Any(tr2 => tr.Id == tr2.Id)));
             }
             else
             {
-                trainees = new List<Trainee>();
+                ToDisplay = new List<Trainee>();
             }
-            list.DataContext = trainees;
+            list.DataContext = ToDisplay;
+        }
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+            if (SearchBar.Text == "")
+            {
+                trainees = new List<Trainee>(bl.GetAllTrainees());
+            }
+            else if (SearchID.IsChecked == true)
+            {
+                trainees = new List<Trainee>();
+                trainees.Add(new Trainee(bl.GetTraineeById(int.Parse(SearchBar.Text))));
+            }
+            else if (SearchName.IsChecked == true)
+            {
+                throw new NotImplementedException();
+            }
+            else if (SearchSchool.IsChecked == true)
+            {
+                var help = new List<List<Trainee>>(from Group in bl.GetTraineesBySchoolName() where Group.Key == SearchBar.Text select Group.ToList());
+                if (help.Count > 1)
+                {
+                    throw new Exception(string.Format("More than one school is named {0}", SearchBar.Text));
+                }
+                if (help.Count == 0)
+                {
+                    trainees = new List<Trainee>();
+                }
+                else
+                {
+                    trainees = help[0];
+                }
+            }
+            else if (SearchTeacher.IsChecked == true)
+            {
+                var help = new List<List<Trainee>>(from Group in bl.GetTraineesByTeacher() where Group.Key == SearchBar.Text select Group.ToList());
+                if (help.Count > 1)
+                {
+                    throw new Exception(string.Format("More than one Teacher is named {0}", SearchBar.Text));
+                }
+                if (help.Count == 0)
+                {
+                    trainees = new List<Trainee>();
+                }
+                else
+                {
+                    trainees = help[0];
+                }
+            }
+
+            ViewLicenseOwners_Click(null, null);
         }
     }
 }
